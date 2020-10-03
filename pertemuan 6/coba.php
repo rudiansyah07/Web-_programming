fca71b055bea448ce0b5cb578911dae20c77d475
rudiansyah07 <rudi.eltibiz07@gmail.com>
2020-10-03 10:11:31 +0800

    koneksi

---
+++ pertemuan 6/coba.php
@@ -0,0 +1,166 @@
+<?php 
+$localhost = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'database-php';
+
+$koneksi = mysqli_connect($localhost, $username, $password, $database);
+
+if (!$koneksi) {
+	die("koneksi gagal: " . mysqli_connect_error());
+} else {
+	echo "koneksi berhasil";
+}
+
+
+if (isset($_POST['submit'])) {
+	$nama = $_POST['nama'];
+	$tempat_lahir = $_POST['tempat_lahir'];
+	$tanggal_lahir = $_POST['tanggal_lahir'];
+	$nik = $_POST['nik'];
+	$email = $_POST['email'];
+	$telp = $_POST['telp'];
+	$telp_wali = $_POST['telp_wali'];
+	$pekerjaan = $_POST['pekerjaan'];
+	$alamat = $_POST['alamat'];
+	$program_akademi = $_POST['program_akademi'];
+	$tema_pelatihan = $_POST['tema_pelatihan'];
+	$mitra_pelatihan = $_POST['mitra_pelatihan'];
+
+	$result = "INSERT INTO registrasi VALUES ('', '$nama', '$tempat_lahir', 'tanggal_lahir', '$nik', '$email', '$telp', 'telp_wali', '$pekerjaan', '$alamat', '$program_akademi', '$tema_pelatihan', '$mitra_pelatihan')";
+
+	$sql = mysqli_query($koneksi, $result);
+
+	if ($sql) {
+		echo "<script>alert('Data berhasil ditambahkan'),</script>";
+	} else {
+		echo "<script>alert('Data gagal ditambahkan'),</script>";
+
+	}
+	
+}
+
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
+</head>
+<body>
+	<center><table rder="1" cellpadding="7" cellspacing="0"><tr><td>
+	<center><table>
+		<tr>
+			<td><center>LAPORAN PERTANGGUNGJAWABAN<BR>
+			PESERTA PROGRAM PEMBEKAJARAN BANTUAN PEMERINTAH<BR>DIGITALTALENT SCHOLARSHIP TAHUN 2020</center></td>
+		</tr>
+</table></center><br>
+<table border="1" cellpadding="7" cellspacing="0">
+		<tr>
+			<td><i>Formulir ini untuk diisi (diketik atau ditulis tangan dengan jelas) kemudian diunggah/diapload di akun digitalent.kominfo.go.id oleh <br>peserta masing-masing di <b>awal</b> pelaksanaan pelatihan</i></td>
+		</tr>
+</table>
+
+<table rder="1" cellpadding="7" cellspacing="0">
+	<form action="" method="POST">
+		<tr>
+			<td>Saya Yang bertandatangan dibawah ini</td>
+			<td> : </td>
+		</tr>
+		<tr>
+			<td><label for="nama">Nama Lengkap (Sesuai KTP)</label></td>
+			<td>:</td>
+			<td><input type="text" name="nama" id="nama" placeholder="Masukan Nama Lengkap" required></td>
+		</tr>
+		<tr>
+			<td><label for="tempat_lahir">Tempat Lahir</label></td>
+			<td>:</td>
+			<td><input type="text" name="tempat_lahir" id="tempat_lahir" placeholder="Masukan Tempat Lahir" required></td>
+		</tr>
+		<tr>
+			<td><label for="tanggal_lahir">Tanggal Lahir</label></td>
+			<td>:</td>
+			<td><input type="date" name= "tanggal_lahir" id="tanggal_lahir" placeholder="Masukan Tanggal Lahir" required></td>
+		</tr>
+		<tr>
+			<td><label for="nik">NIK/NIP</label></td>
+			<td>:</td>
+			<td><input type="text" name="nik" id="nik" placeholder="Masukan Nomor NIK" required></td>
+		</tr>
+		<tr>
+			<td><label for="email">E-Mail Aktif</label></td>
+			<td>:</td>
+			<td><input type="text" name="email" id="email" placeholder="Masukan Alamat E-Mail" required></td>
+		<tr>
+			<td><label for="telp "><N>No. HP Aktif</N></label></td>
+			<td>:</td>
+			<td><input type="number" name="telp " id="telp " placeholder="Masukan Nomor HP" required></td>
+		</tr>
+		<tr>
+			<td><label for="telp_wali">No HP (Wali/Atasan)</label></td>
+			<td>:</td>
+			<TD><input type="number" name="telp_wali" id="no_wali" placeholder="Masukan Nomor HP Wali" required></td>
+		</tr>
+		<tr>
+			<td><label for="pekerjaan">Pekerjaan</label></td>
+			<td>:</TD>
+			<td><input type="text" name="pekerjaan" id="pekerjaan" placeholder="Masukan Pekerjaan" required></td>
+		</tr>
+		<tr>
+			<td><label for="alamat">Alamat Domisili</label></td>
+			<td>:</td>
+			<td><textarea name="alamat" id="alamat" cols="40" rows="5" placeholder="Masukan Alamat Rumah" required></textarea></td>
+		</tr>
+		<td><label for="program_akademi">Program Akademi</label></td>
+			<td>:</td>
+			<td><input type="text" name="program_akademi" id="program_akademi" placeholder="Masukan Program Akademi"required></td>
+		</tr>
+		</tr>
+		<td><label for="tema">Tema Pelatihan</label></td>
+			<td>:</td>
+			<td><input type="text" name="tema_pelatihan" id="tema" placeholder="Masukan Tema Pelatihan" required></td>
+		</tr>
+		</tr>
+		<td><label for="mitra">Mitra Pelatihan<br></label></td>
+			<td>:</td>
+			<td><input type="text" name="mitra_pelatihan" id="mitra" placeholder="Masukan Mitra Pelatihan"><br>
+				<button type="submit" name="submit">Kirim</button> | <button type="button">Batal</button></td>
+		</tr>
+	</table>
+</form>
+
+<table border="1" cellpadding="7" cellspacing="0">
+	<tr>
+		<td>no</td>
+		<td>Nama</td>
+		<td>Tempat Lahir</td>
+		<td>Tgl Lahir</td>
+		<td>No. HP</td>
+		<td>Email</td>
+		<td>Aksi</td>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td>M. Iqbal Adenan</td>
+		<td>Kuala Kapuas</td>
+		<td>17 Oktober 1992</td>
+		<td>085249099652</td>
+		<td>kumpultugas17@gmail.com</td>
+		<td><button>Edit</button> <button>Hapus</button></td>
+	</tr>
+		<tr>
+		<td>2</td>
+		<td>Agus Setiawan</td>
+		<td>Banjarmasin>
+		<td>17 Oktober 1992</td>
+		<td>087843969674</td>
+		<td>Agus@gmail.com</td>
+		<td><button>Edit</button> <button>Hapus</button></td>
+	</tr>
+</table></center>
+	
+
+</body>
---
+++ pertemuan 6/latihan 1 (Array).php
@@ -0,0 +1,14 @@
+<?php 
+$x = array('Rudiansyah', '082252963721', 'rudi.eltibiz07@gmail.com' );
+echo "Nama : ".$x[0];
+echo "<br>WhatApp : ".$x[1];
+echo "<br>Email : ".$x[2];
+
+
+$y = array('nama' =>'Rudiansyah' , 'wa' => '082252963721', 'email' => 'rudi.eltibiz07@gmail.com' );
+echo "<br><br>";
+echo"Nama : ".$y['nama'];
+echo"<br>WhatApp : ".$y['wa'];
+echo"<br>Email : ".$y['email'];
+
+ ?>
